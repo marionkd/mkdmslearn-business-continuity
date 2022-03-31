2a67069b8ad5d53fb727d05c7196a1fad87d8887
marionkd <91678100+marionkd@users.noreply.github.com>
2022-03-31 12:20:30 +0100

    Update and rename setup.sh to setup.ps1

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-$resource = (Get-AzResourceGroup)[0];
-
-echo "Creating VM..."
-az vm create --name labvm --image UbuntuLTS --admin-username azureuser --generate-ssh-keys --resource-group $resource
-
-printf "***********************  Lab Environment Created  *********************\n\n"
