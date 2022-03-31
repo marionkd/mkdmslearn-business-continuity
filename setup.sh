887a8e88e889b4a8781160cf43d5c85cf8c9be5b
marionkd <91678100+marionkd@users.noreply.github.com>
2022-03-31 10:59:15 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-!/bin/bash
-echo "Creating VM..." 
-az vm create --name labvm --image UbuntuLTS --admin-username azureuser --generate-ssh-keys --resource-group demo
+#!/bin/bash
+resource=`az group list --query '[0].name' --output tsv`
 
-printf "*********************** Lab Environment Created *********************\n\n"
+echo "Creating VM..."
+az vm create --name labvm --image UbuntuLTS --admin-username azureuser --generate-ssh-keys --resource-group $resource
+
+printf "***********************  Lab Environment Created  *********************\n\n"
