ebd73e68f6b1bef22a0977d2fddebcc645ebedcd
marionkd <91678100+marionkd@users.noreply.github.com>
2022-03-31 11:07:20 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$resource=`az group list --query '[0].name' --output tsv`
+$resource = (Get-AzResourceGroup)[0];
 
 echo "Creating VM..."
 az vm create --name labvm --image UbuntuLTS --admin-username azureuser --generate-ssh-keys --resource-group $resource
